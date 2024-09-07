8d347592364c6f579c38cefda1f37bf65f4ceca5
Aayush Rai <coolaayushrai@gmail.com>
2024-09-07 16:34:14 +0530

    Create slider functions to move image slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const CarouselSlider = function(){
+    const imageReel = document.querySelector('.images');
+
+    const next = () => {
+        let initialPosition = parseInt(imageReel.style.getPropertyValue('left'));
+        let newPosition = initialPosition - 100;
+        imageReel.style.setProperty('left', `${newPosition}%`);
+    };
+
+    const previous = () => {
+        let initialPosition = parseInt(imageReel.style.getPropertyValue('left'));
+        let newPosition = initialPosition + 100;
+        imageReel.style.setProperty('left', `${newPosition}%`);
+    };
+
+    return { next, previous };
+}();
